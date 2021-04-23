78de500269f9e2344486280aa15f8ff1fdbf7cee
Islam Nabil <17936522+islamnabil@users.noreply.github.com>
2021-04-23 21:32:15 +0200

    Add ImagePicker to NoteDetailsVC

--- SmartNotepad/SmartNotepad/Controllers/NoteDetailsVC.swift
+++ SmartNotepad/SmartNotepad/Controllers/NoteDetailsVC.swift
@@ -36,6 +36,32 @@ class NoteDetailsVC: UIViewController {
         }
     }
     
+    fileprivate func setNoteDetails(){
+        noteTitleTextField.text = note?.title
+        noteBodyTextView.text = note?.body
+        if note?.locationAddress != "" {
+            setAddress(address: note?.locationAddress ?? "")
+        }
+        if note?.image != "" {
+            setNoteImage(imageName: URL(string: note?.image ?? "")!)
+        }
+    }
+    
+    fileprivate func setAddress(address:String){
+        addLocationButton.setTitle(address, for: .normal)
+        addLocationButton.setTitleColor(.black, for: .normal)
+    }
+    
+    fileprivate func setNoteImage(imageName:URL){
+        let image = loadImage(fileURL: imageName)
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 60, y: 10, width: 200, height: 100)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.cornerRadius = 5
+        addPhotoView.addSubview(imageView)
+    }
+    
     fileprivate func setupLocation(){
         self.locationManager.requestWhenInUseAuthorization()
         if CLLocationManager.locationServicesEnabled() {
@@ -49,38 +75,12 @@ class NoteDetailsVC: UIViewController {
             @unknown default:
                 break
             }
-           
         }
     }
     
-    fileprivate func setNoteDetails(){
-        noteTitleTextField.text = note?.title
-        noteBodyTextView.text = note?.body
-        if note?.locationAddress != "" {
-            setAddress(address: note?.locationAddress ?? "")
-        }
-        if note?.image != "" {
-            setNoteImage(imageName: note?.image ?? "")
-        }
-    }
     
-    fileprivate func setAddress(address:String){
-        addLocationButton.setTitle(address, for: .normal)
-        addLocationButton.setTitleColor(.black, for: .normal)
-    }
-    
-    fileprivate func setNoteImage(imageName:String){
-        let image =  UIImage(named: imageName)
-        let imageView = UIImageView(image: image)
-        imageView.frame = CGRect(x: 60, y: 10, width: 200, height: 100)
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.cornerRadius = 5
-        addPhotoView.addSubview(imageView)
-    }
-    
-    // MARK: - ShowActionSheet
-    func showEnableLocationServices() {
+    // MARK: - showEnableLocationServices
+    fileprivate func showEnableLocationServices() {
         let alert = UIAlertController(title: "", message: "Enable location Services ?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (_) in
             guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
@@ -114,6 +114,7 @@ class NoteDetailsVC: UIViewController {
     }
     
     @IBAction func addPhoto(_ sender: Any) {
+        self.showAlert()
     }
     
 }
@@ -135,6 +136,7 @@ extension NoteDetailsVC:UITextViewDelegate {
     }
 }
 
+// MARK: - CLLocationManagerDelegate
 extension NoteDetailsVC: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -151,4 +153,69 @@ extension NoteDetailsVC: CLLocationManagerDelegate {
         self.setAddress(address: "Failed to fetch address")
     }
     
+}
+
+//MARK:- Image Picker
+extension NoteDetailsVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    //Show alert to selected the media source type.
+    private func showAlert() {
+
+        let alert = UIAlertController(title: "Image Selection", message: "From where you want to pick this image?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+            self.getImage(fromSourceType: .camera)
+        }))
+        alert.addAction(UIAlertAction(title: "Photo Album", style: .default, handler: {(action: UIAlertAction) in
+            self.getImage(fromSourceType: .photoLibrary)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    //get image from source type
+    private func getImage(fromSourceType sourceType: UIImagePickerController.SourceType) {
+
+        //Check is source type available
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = sourceType
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+    }
+
+    //MARK:- UIImagePickerViewDelegate.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imgUrl = info[UIImagePickerController.InfoKey.imageURL] as? URL{
+               let imgName = imgUrl.lastPathComponent
+               let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+               let localPath = documentDirectory?.appending(imgName)
+
+            let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+            let data = image.pngData()! as NSData
+               data.write(toFile: localPath!, atomically: true)
+               let photoURL = URL.init(fileURLWithPath: localPath!)
+               print(photoURL)
+            self.setNoteImage(imageName: photoURL)
+           }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func loadImage(fileURL: URL) -> UIImage? {
+        do {
+            let imageData = try Data(contentsOf: fileURL)
+            return UIImage(data: imageData)
+        } catch {
+            print("Error loading image : \(error)")
+        }
+        return nil
+    }
+
+
 }
