3046adf00682ffaccf6e90cfeef2d181d46350cb
Islam Nabil <17936522+islamnabil@users.noreply.github.com>
2021-04-23 17:59:30 +0200

    Add location services to NoteDetailsVC

--- SmartNotepad/SmartNotepad/Controllers/NoteDetailsVC.swift
+++ SmartNotepad/SmartNotepad/Controllers/NoteDetailsVC.swift
@@ -6,25 +6,111 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NoteDetailsVC: UIViewController {
+    // MARK: - Properties
+    fileprivate var note:NoteModel?
+    fileprivate var locationManager = CLLocationManager()
+    
     // MARK: - IBOutlets
     @IBOutlet weak var noteTitleTextField: UITextField!
     @IBOutlet weak var noteBodyTextView: UITextView!
+    @IBOutlet weak var addPhotoViewHeightContraint: NSLayoutConstraint!
+    @IBOutlet weak var addPhotoView: UIView!
+    @IBOutlet weak var addLocationButton: UIButton!
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        noteBodyTextView.delegate = self
-        noteBodyTextView.textColor = UIColor.lightGray
+        setupViews()
     }
     
     // MARK: - Private Methods
+    fileprivate func setupViews(){
+        noteBodyTextView.delegate = self
+        noteBodyTextView.textColor = UIColor.lightGray
+        noteTitleTextField.attributedPlaceholder = NSAttributedString(string: "Note Title Here",attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        if note != nil {
+            setNoteDetails()
+        }
+    }
+    
+    fileprivate func setupLocation(){
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            switch locationManager.authorizationStatus {
+            case .restricted, .denied:
+                showEnableLocationServices()
+            case .authorizedAlways, .authorizedWhenInUse, .notDetermined:
+                locationManager.delegate = self
+                locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                locationManager.startUpdatingLocation()
+            @unknown default:
+                break
+            }
+           
+        }
+    }
+    
+    fileprivate func setNoteDetails(){
+        noteTitleTextField.text = note?.title
+        noteBodyTextView.text = note?.body
+        if note?.locationAddress != "" {
+            setAddress(address: note?.locationAddress ?? "")
+        }
+        if note?.image != "" {
+            setNoteImage(imageName: note?.image ?? "")
+        }
+    }
+    
+    fileprivate func setAddress(address:String){
+        addLocationButton.setTitle(address, for: .normal)
+        addLocationButton.setTitleColor(.black, for: .normal)
+    }
+    
+    fileprivate func setNoteImage(imageName:String){
+        let image =  UIImage(named: imageName)
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 60, y: 10, width: 200, height: 100)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.cornerRadius = 5
+        addPhotoView.addSubview(imageView)
+    }
+    
+    // MARK: - ShowActionSheet
+    func showEnableLocationServices() {
+        let alert = UIAlertController(title: "", message: "Enable location Services ?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (_) in
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                     return
+                }
+                 if UIApplication.shared.canOpenURL(settingsUrl) {
+                     UIApplication.shared.open(settingsUrl, completionHandler: { (success) in })
+                  }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: false, completion: nil)
+    }
+    
     
     // MARK: - Public Methods
+    func setData(note:NoteModel){
+        self.note = note
+    }
     
     // MARK: - IBActions
+    @IBAction func saveNote(_ sender: Any) {
+        
+    }
+    
     @IBAction func addLocation(_ sender: Any) {
+        setupLocation()
     }
     
     @IBAction func addPhoto(_ sender: Any) {
@@ -47,4 +133,22 @@ extension NoteDetailsVC:UITextViewDelegate {
             textView.textColor = UIColor.lightGray
         }
     }
+}
+
+extension NoteDetailsVC: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.first != nil {
+            CLGeocoder().reverseGeocodeLocation(manager.location ?? CLLocation()) { (placemarks, err) in
+                if placemarks?.count ?? 0 > 0 {
+                    self.setAddress(address: "\(placemarks?[0].locality ?? ""), \(placemarks?[0].country ?? "")")
+                }
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.setAddress(address: "Failed to fetch address")
+    }
+    
 }
